b122b55b5d277e1e5d7bc589cc5b5f5d24a20007
stiven <stivenchoo@gmail.com>
2024-04-25 10:20:42 -0500

    add tokens

--- backend/usuarios/verificarSesion.php
+++ backend/usuarios/verificarSesion.php
@@ -1,10 +1,32 @@
 <?php
-require_once('../cors.php');
-session_start();
 
-$response = array();
-$response['sessionActive'] = isset($_SESSION['usuario_id']); // Verificar si existe la sesión
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
-header('Content-Type: application/json');
-echo json_encode($response);
-?>
+require_once '../vendor/autoload.php';
+require_once '../cors.php';
+
+try {
+    // Leer la solicitud POST y obtener el token JWT del cuerpo de la solicitud
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, true);
+    $token = $input['token'];
+
+    // Clave secreta utilizada para firmar y verificar el token (asegúrate de usar la misma clave que usaste para generar el token)
+    $secret = "123";
+
+    // Decodificar el token para obtener su contenido
+    $decoded = JWT::decode($token, new Key('123', 'HS256'));
+    // Verificar si el token es válido y contiene la información esperada
+    if ($decoded->user_id) {
+        // El token es válido y contiene el ID del usuario
+        // Puedes realizar acciones adicionales aquí si lo deseas
+        echo json_encode(["sessionActive" => true]);
+    } else {
+        // El token no es válido o no contiene la información esperada
+        echo json_encode(["sessionActive" => false]);
+    }
+} catch (Exception $e) {
+    // Ocurrió un error al decodificar el token
+    echo json_encode(["sessionActive" => false]);
+}
